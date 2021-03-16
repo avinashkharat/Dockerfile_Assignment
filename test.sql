85500a76bc6f8f4ee53da96e30c2bee6755d4d81
avinashkharat <59007918+avinashkharat@users.noreply.github.com>
2021-03-16 21:59:57 +0530

    Add files via upload

---
+++ test.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE IF NOT EXISTS pucsdStudent;
+
+CREATE USER IF NOT EXISTS 'pucsd'@'localhost' IDENTIFIED BY 'pucsd';
+
+GRANT ALL PRIVILEGES ON pucsdStudent. * TO 'pucsd'@'localhost';
+
+DROP TABLE IF EXISTS studentData;
+
+CREATE TABLE studentData
+(
+Name char(56),
+Roll_no int,
+Address varchar(40),
+Mobile varchar(10),
+PAN_Number varchar(10)
+);
+
+INSERT INTO studentData VALUES('Prash Kant',191212,'Nashik','9545776589','AORPY1223C');
+INSERT INTO studentData VALUES('Vin Palv',191213,'Pune','9645776589','AORPY1663C');
+INSERT INTO studentData VALUES('Yash Rale',191214,'Ahmednagar','9745776589','AORPY1224Z');
+INSERT INTO studentData VALUES('Ved Gadge',191215,'Nashik','9945776589','AORPY1223C');
